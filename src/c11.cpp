51c3f4ff70ac1d07ac3a33b9e51426f3a0cd5fda
bencode <bencode@163.com>
2018-11-08 19:37:21 +0800

    finish

---
+++ src/c11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define i int day
+#define meet =
+#define you 1
+#define die ((day <= 1000))
+
+
+int main() {
+  for (i meet you; die; day++) {
+    printf("I Love You");
+  }
+}
