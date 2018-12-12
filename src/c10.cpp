78d6bb77a3db7fcc7a42e6f20c560056b58c5c82
bencode <bencode@163.com>
2018-12-12 20:11:19 +0800

    update

--- src/c10.cpp
+++ src/c10.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 
 int main() {
-  char strs[3][100];
-  for (int i = 0; i < 3; i++) {
-    cin >> strs[i];   // c++中可以使用这个输入，C语言中使用 gets
-  }
+  char *strs[] = {"Hello", "World", "abcd"};
+  char **ppstrs = strs;
 
-  // 下面就一样了
-
-  char *min = strs[0];
+  char *min = *ppstrs;   // 相当于 *min = ppstrs[0]
   for (int i = 1; i < 3; i++) {
-    if (strcmp(min, strs[i]) > 0) {
-      min = strs[i];
+    if (strcmp(min, *(ppstrs + i)) > 0) { // 相当于if (strcmp(min, ppstrs[i]) > 0) {
+      // min = *(ppstrs + i);    用这句也行，其实和下面的一样的
+      min = ppstrs[i];
     }
   }
 
